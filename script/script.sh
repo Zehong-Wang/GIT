146233c2f89de2efde2064e62b7c3e81c1aaf0f1
Zehong <zwang43@nd.edu>
2024-09-08 14:28:29 -0400

    debug node zero-shot on mini-batch

--- script/script.sh
+++ script/script.sh
@@ -9,7 +9,16 @@
 
 
 
-for dataset in codex_s codex_m codex_l NELL995 GDELT ICEWS1819
+# for dataset in cora citeseer pubmed arxiv arxiv23 dblp bookhis bookchild elecomp elephoto sportsfit amazonratings products chempcba chemhiv bbbp bace toxcast cyp450 tox21 muv WN18RR FB15K237 codex_s codex_m codex_l NELL995 GDELT ICEWS1819
+
+for dataset in chempcba chemhiv bbbp bace toxcast cyp450 tox21 muv
 do
-    ~/anaconda3/envs/OFA/bin/python ../finetune.py --setting in_context --pretrain_data default --dataset $dataset 
+    for setting in in_context zero_shot
+    do
+        for pt_lr in 1e-5 5e-6 1e-6 1e-7
+        do
+            ~/.conda/envs/OFA/bin/python finetune.py --pt_data default --sft_data na --setting $setting --dataset $dataset --exp_group pretrain-parameter-tuning_pt_lr --pt_lr $pt_lr
+        done
+        ~/.conda/envs/OFA/bin/python finetune.py --pt_data na --sft_data na --setting $setting --dataset $dataset --exp_group without-pretrain
+    done
 done
